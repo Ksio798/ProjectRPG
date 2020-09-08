11c2fc30ff796f9face988ee9f82330297bb0092
Ksio798 <66674663+Ksio798@users.noreply.github.com>
2020-09-08 21:00:52 +1000

    начало нового врага

---
+++ Assets/Scripts/Enemies/TeletortingEnemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeletortingEnemy : EnemyController
+{
+    protected override void Attack()
+    {
+        if (timer >= TimeToAttack)
+        {
+            followTarget.GetComponent<PlayerController>().TakeDamage(stats.Damage);
+            timer = 0;
+            Teleport();
+        }
+    }
+    void Teleport()
+    {
+
+    }
+}
