f23be4ace7a257b982a577d32cc66de7c7a930a7
Ksio798 <66674663+Ksio798@users.noreply.github.com>
2020-09-29 18:52:12 +1000

    Главное меню

---
+++ Assets/Scripts/UI/MainManuController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainManuController : MonoBehaviour
+{
+    public GameObject SavePanel;
+    public GameObject SettingsPanel;
+   public void LoadS(int index)
+    {
+        StartCoroutine(WaitToLoad(index));
+    }
+    public void SavePanelA()
+    {
+        SavePanel.SetActive(!SavePanel.activeSelf);
+        SettingsPanel.SetActive(false);
+    }
+    public void SettingsPanelA()
+    {
+        SettingsPanel.SetActive(!SettingsPanel.activeSelf);
+        SavePanel.SetActive(false);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    IEnumerator WaitToLoad(int index)
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(index);
+    }
+   
+
+}
---
+++ Assets/Scripts/UI/MainMenuButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class MainMenuButton : MonoBehaviour, IPointerEnterHandler
+{
+    public AudioSource clip;
+    public AudioSource clip2;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        clip.Play();
+    }
+    public void OnClick()
+    {
+        clip2.Play();
+    }
+
+}
