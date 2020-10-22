00e5ca716afab1b45ee2dfa389ad3c37c5445f24
Chlima1 <68585386+Chlima1@users.noreply.github.com>
2020-10-22 18:34:55 +1000

    Dershi

--- Assets/Scripts/AttackSystem/AttackMethods/Weapons/TurretScript.cs
+++ Assets/Scripts/AttackSystem/AttackMethods/Weapons/TurretScript.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class TurretScript : MonoBehaviour
 {
     Transform target;
+    public ViewZone viewZone;
+    public string TargetTag;
+    public TurretShooting turret;
+    public int damage;
     // Start is called before the first frame update
     void Start()
     {
-        
+        viewZone.OnObjectEnterZone += View;
     }
 
     // Update is called once per frame
@@ -19,5 +23,23 @@ public class TurretScript : MonoBehaviour
     void Attack()
     {
         Vector3 direction = target.position - transform.position;
+        turret.Shoot(direction, damage, TargetTag);
+    }
+    void View(Transform other, bool enter)
+    {
+        if (enter)
+        {
+            if (other.tag == TargetTag)
+            { 
+            target = other;
+            Attack();
+            }
+        }
+        else
+        {
+            if (target == other)
+                target = null;
+        }
+        
     }
 }
