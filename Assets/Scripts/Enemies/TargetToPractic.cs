067b14288a3ceb1a608daa297f3347a6d074cb6c
Ksio798 <66674663+Ksio798@users.noreply.github.com>
2020-11-05 18:29:30 +1000

    Первый уровень #3

---
+++ Assets/Scripts/Enemies/TargetToPractic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TargetToPractic : BaseCharecter
+{
+    public Image image;
+    public override void TakeDamage(float Dmg)
+    {
+        base.TakeDamage(Dmg);
+        UpdateHp();
+    }
+
+    void UpdateHp()
+    {
+        image.fillAmount = stats.health / stats.MaxHealth;
+    }
+}
--- Assets/Scripts/ObjectViewZone.cs
+++ Assets/Scripts/ObjectViewZone.cs
@@ -8,11 +8,13 @@ public class ObjectViewZone : MonoBehaviour
     public event System.Action OnObjExitZone;
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if(other.tag =="Player")
         OnObjEnterZone?.Invoke();
     }
     private void OnTriggerExit2D(Collider2D other)
     {
-        OnObjExitZone?.Invoke();
+        if (other.tag == "Player")
+            OnObjExitZone?.Invoke();
     }
 
 }
