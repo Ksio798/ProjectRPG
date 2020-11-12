431f4d463f647e9bd847b0b786bb10338a6da12a
Chlima1 <68585386+Chlima1@users.noreply.github.com>
2020-11-12 18:10:34 +1000

    персонажы
    готовы

--- Assets/Scripts/Skills/AlexBariier.cs
+++ Assets/Scripts/Skills/AlexBariier.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class AlexBariier : AttackMethod
 {
     public GameObject Barrier;
+    public GameObject BarrierMask;
 
     public GameObject CrossHair;
 
     public override void OnFire(Stats playerStats)
     {
-        GameObject newBarrier = Instantiate(Barrier);
+        if (playerStats.manna > 0)
+        {
+            GameObject newBarrier = Instantiate(Barrier);
+            playerStats.manna -= 20;
+            FireAttack?.Invoke();
+            Destroy(newBarrier.gameObject, 5);
+        }
     }
     public void Update()
     {
         Barrier.transform.position = CrossHair.transform.position;
     }
+   
 }
