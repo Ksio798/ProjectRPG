9d476b9daabc6c4f8a456151e59517c49bdff9b2
Ksio798 <66674663+Ksio798@users.noreply.github.com>
2020-12-24 18:11:36 +1000

    Босс Егор

---
+++ Assets/Scripts/Enemies/BossEgor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEgor : EnemyController
+{
+    public bool Activated = false;
+
+
+    public void AtivateBoss()
+    {
+        Activated = true;
+    }
+         
+}
